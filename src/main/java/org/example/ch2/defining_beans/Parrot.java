043bbe8495d3812baa361b2cb16c055d7d397874
carlosfrmax <39538657+carlosfrmax@users.noreply.github.com>
2021-12-29 23:39:55 +0100

    Revert "cleaning project"
    This reverts commit 2dc12471234d9fa7950a4ec34a80945bec5dd75b.

---
+++ src/main/java/org/example/ch2/defining_beans/Parrot.java
@@ -0,0 +1,20 @@
+package org.example.ch2.defining_beans;
+
+import javax.annotation.PostConstruct;
+
+public class Parrot {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.name = "Kiki";
+    }
+}
---
+++ src/main/java/org/example/ch2/defining_beans/ProjectConfig.java
@@ -0,0 +1,64 @@
+package org.example.ch2.defining_beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.ch2.defining_beans")
+public class ProjectConfig {
+
+    @Bean
+    Horse horse() {
+        var h = new Horse();
+        h.setName("Sugar");
+        return h;
+    }
+
+    @Bean
+    String hello() {
+        return "Hello";
+    }
+
+    @Bean
+    Integer ten() {
+        return 10;
+    }
+
+    @Bean
+    Cat cat1() {
+        var c = new Cat();
+        c.setName("Tom");
+        return c;
+    }
+
+    @Bean
+    Cat cat2() {
+        var c = new Cat();
+        c.setName("Max");
+        return c;
+    }
+
+    @Bean
+    Parrot parrot1() {
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    @Primary
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Riki");
+        return p;
+    }
+}
